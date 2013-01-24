93ab4182e4915bcc4e6a488922accf367d969c9c
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2013-01-24 14:08:38 +0100

    using iterator instead of list

--- euler.py
+++ euler.py
@@ -1,7 +1,7 @@
 from time import time
 
 def euler004():
-    print max(filter(lambda x: str(x)==str(x)[::-1], [x*y for x in range(100,1000) for y in range(100, 1000)] ))
+    print max(filter(lambda x: str(x)==str(x)[::-1], (x*y for x in range(100,1000) for y in range(100, 1000)) ))
 
 
 if __name__ == '__main__' :
