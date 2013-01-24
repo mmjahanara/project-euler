680f9b39a2144c4a4815a41484a9061a52e61425
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2013-01-24 21:07:42 +0100

    added Scala solution for question 2

--- euler.scala
+++ euler.scala
@@ -1,15 +1,23 @@
 object Euler {
     def euler001() {
-      var rg = List.range(1,1000) 
+      val rg = List.range(1,1000) 
       println (rg.filter(x => x%3==0 || x%5 ==0).reduce(_+_))
+    }
+    def euler002() {
+      var a = 1; var b = 1; var c = 2; var sum = 0
+      while (c <= 4000000) {
+          sum = sum +c
+          a = b+c; b = c+a; c=a+b; 
+      }
+      println (sum)
     }
     def euler004() {
       val a = List.range(100,1000)
-      (for { i<- a; j<-a} yield i*j) filter(x=>x.toString==x.toString.reverse) max 
+      println ((for { i<- a; j<-a} yield i*j) filter(x=>x.toString==x.toString.reverse) max)
     }
     def main(args: Array[String]) {
         var a = System.currentTimeMillis()
-        euler004()
+        euler002()
         println ("time elapsed: " + (System.currentTimeMillis()-a) + " millisec")
     }
 }
