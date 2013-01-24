e5098c98209398432ec5928876ce8a33d95229da
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2013-01-24 13:41:02 +0100

    scala solution problem 4

--- euler.scala
+++ euler.scala
@@ -3,9 +3,13 @@ object Euler {
       var rg = List.range(1,1000) 
       println (rg.filter(x => x%3==0 || x%5 ==0).reduce(_+_))
     }
+    def euler004() {
+      val a = List.range(100,1000)
+      (for { i<- a; j<-a} yield i*j) filter(x=>x.toString==x.toString.reverse) max 
+    }
     def main(args: Array[String]) {
         var a = System.currentTimeMillis()
-        euler001()
+        euler004()
         println ("time elapsed: " + (System.currentTimeMillis()-a) + " millisec")
     }
 }
