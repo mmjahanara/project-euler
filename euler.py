cc210c8035f9118b625648355248abb03867efd5
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2013-01-24 14:26:45 +0100

    correct time calculation

--- euler.py
+++ euler.py
@@ -1,11 +1,11 @@
-from time import time
+import time
 
 def euler004():
     print max(filter(lambda x: str(x)==str(x)[::-1], (x*y for x in range(100,1000) for y in range(100, 1000)) ))
 
 
 if __name__ == '__main__' :
-    a = time()
+    a = time.time()
     euler004()
-    print "time elapsed: %f millisec" % (time()-a)
+    print "time elapsed: %f sec" % (time.time()-a)
 
