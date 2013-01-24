a447b59959faf75605fbe5851229818e6bca9d8a
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2013-01-24 14:05:22 +0100

    python solution for question 4

---
+++ euler.py
@@ -0,0 +1,11 @@
+from time import time
+
+def euler004():
+    print max(filter(lambda x: str(x)==str(x)[::-1], [x*y for x in range(100,1000) for y in range(100, 1000)] ))
+
+
+if __name__ == '__main__' :
+    a = time()
+    euler004()
+    print "time elapsed: %f millisec" % (time()-a)
+
