75d8cad1e33d6525b50a54c948cdc615d89416a0
Gefei Zhang <gefei.zhang@pst.ifi.lmu.de>
2013-09-04 18:25:11 +0200

    updated python 059

--- 059.py
+++ 059.py
@@ -1,27 +1,27 @@
+import re
+
 def e059():
     with open('data/cipher1.txt') as f:
         ln = f.readline()
     cipher = map(int, ln.split(','))
     fac = len(cipher)/3 + 1
 
-    #etao = 'ETAOINSHRDLU'
+    with open('data/dict_en.txt') as f:
+        ln = f.readlines()
+    words = {x[:-1].upper() for x in ln}
+
     rg = range(ord('a'), ord('z')+1)
-    fout = open('out.txt', 'w')
 
     for a in rg:
         for b in rg:
             for c in rg:
                 key = ([a,b,c]*fac)[:1201]
-                decipher = [chr(x[0]^x[1]) for x in zip(key, cipher)]
-                dcstr = ''.join(decipher)
-                upp = dcstr.upper()
-                cntr = Counter(upp)
-                if (upp.find('THE') < 0): continue
-                if (upp.find('AND') < 0): continue
-                if (upp.find('OF') < 0): continue
-                if (upp.find('IN') < 0): continue
-                fout.write(dcstr+'\n')
-    fout.close()
+                decipher = ''.join([chr(x[0]^x[1]) for x in zip(key, cipher)])
+                up_list = re.sub(r'\W', ' ', decipher.upper()).split()
+                x = [1 if w in words else 0 for w in up_list]
+                if sum(x) < .8 * len(up_list): continue
+                print decipher 
+                print sum(map(ord, decipher))
 
 if __name__ == '__main__':
     e059()
