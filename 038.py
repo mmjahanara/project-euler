c9df54a3356b45ae0b80e4b71997dd8a552d7742
gefei <gefei.zhang@pst.ifi.lmu.de>
2015-03-07 15:35:31 +0100

    update 038

--- 038.py
+++ 038.py
@@ -1,13 +1,13 @@
 import time
 
 def euler038():
-    for n1 in range(9876, 5677, -1):
+    for n1 in range(9487, 9183, -1):
         n2 = n1 + n1
         n1s = set(x for x in str(n1))
         n2s = set(x for x in str(n2))
         s = n1s.union(n2s)
-        if len(s) == 9 and '0' not in s: return n1, n2
-    return 0
+        if len(s) == 9 and '0' not in s: return '%d%d' % (n1, n2)
+    return 918273645 
 
 
 if __name__ == '__main__' :
